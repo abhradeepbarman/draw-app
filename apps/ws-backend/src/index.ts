87cfd326802a9fcf42662ff020a2a6be6814faf3
abhradeepbarman <abhradeep709@gmail.com>
2025-04-10 20:12:08 +0530

    gated ws backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,11 +1,23 @@
 import { WebSocketServer } from "ws";
 import config from "@repo/backend-common/config";
+import jwt from "jsonwebtoken";
 
 const wss = new WebSocketServer({ port: Number(config.PORT) });
 
-wss.on("connection", function connection(ws) {
-    console.log("connected");
-    ws.on("error", console.error);
+wss.on("connection", function connection(ws, request) {
+    const url = request.url;
+    if (!url) {
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split("?")[1]);
+    const token = queryParams.get("token");
+    const decoded = jwt.verify(token!, config.JWT_SECRET);
+
+    if (!decoded) {
+        ws.close();
+        return;
+    }
 
     ws.on("message", function message(data) {
         console.log("received: %s", data);
